00efdf223a5ed8b670cb518097b0a62e4cfd74d0
Phuc <phuctv1112004@gmail.com>
2023-08-25 10:40:16 +0700

    Create Document,Exam class and set relationship between entities

--- backend/SmartEdu/Entities/Student.cs
+++ backend/SmartEdu/Entities/Student.cs
@@ -13,5 +13,13 @@ namespace SmartEdu.Entities
         [ForeignKey("Parent")]
         public int ParentId { get; set; }
         public Parent Parent { get; set; } // moi hoc sinh chi thuoc ve 1 phu huynh
+        [ForeignKey("MainClass")]
+
+        public MainClass MainClassId { get; set; }
+        public MainClass MainClass { get; set; }
+
+        public ICollection<ExtraClass> ExtraClasses { get; set; }
+
+        public ICollection<Exam> Exams { get; set; }
     }
 }
--- backend/SmartEdu/Entities/Teacher.cs
+++ backend/SmartEdu/Entities/Teacher.cs
@@ -8,5 +8,11 @@ namespace SmartEdu.Entities
         [ForeignKey("User")]
         public string UserId { get; set; }
         public User User { get; set; }
+        [ForeignKey("MainClass")]
+        public int MainClassId { get; set; }
+        public MainClass MainClass { get; set; }
+        public ICollection<ExtraClass> ExtraClasses { get; set; }
+        public ICollection<Document> Documents { get; set; }
+
     }
 }
