c227df05ac783aa0ad2ffac170160ff7d9435870
Nguyen Huong <nguyenhuongg1104@gmail.com>
2023-08-26 15:01:51 +0700

    Seeding data in ExtraClasses table

--- backend/SmartEdu/Migrations/20230826072747_TempRemoveExtraClassesFromTeacher.cs
+++ backend/SmartEdu/Migrations/20230826072747_TempRemoveExtraClassesFromTeacher.cs
@@ -20,15 +20,15 @@ namespace SmartEdu.Migrations
                 name: "IX_ExtraClasses_TeacherId",
                 table: "ExtraClasses");
 
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "b0c43766-5c55-4a82-a9ac-91d04eb79cef");
+            //migrationBuilder.DeleteData(
+            //    table: "AspNetRoles",
+            //    keyColumn: "Id",
+            //    keyValue: "b0c43766-5c55-4a82-a9ac-91d04eb79cef");
 
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "df899bfe-f156-4cd2-8872-14c02fe24166");
+            //migrationBuilder.DeleteData(
+            //    table: "AspNetRoles",
+            //    keyColumn: "Id",
+            //    keyValue: "df899bfe-f156-4cd2-8872-14c02fe24166");
 
             migrationBuilder.DropColumn(
                 name: "TeacherId",
@@ -44,14 +44,14 @@ namespace SmartEdu.Migrations
                 type: "int",
                 nullable: true);
 
-            migrationBuilder.InsertData(
-                table: "AspNetRoles",
-                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[,]
-                {
-                    { "b0c43766-5c55-4a82-a9ac-91d04eb79cef", "6306f168-ffd9-4fcd-903b-eb2aebdb8a7b", "Admin", "ADMIN" },
-                    { "df899bfe-f156-4cd2-8872-14c02fe24166", "bd03de2d-8858-4faf-bda1-6f37a0ee9919", "User", "USER" }
-                });
+            //migrationBuilder.InsertData(
+            //    table: "AspNetRoles",
+            //    columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+            //    values: new object[,]
+            //    {
+            //        { "b0c43766-5c55-4a82-a9ac-91d04eb79cef", "6306f168-ffd9-4fcd-903b-eb2aebdb8a7b", "Admin", "ADMIN" },
+            //        { "df899bfe-f156-4cd2-8872-14c02fe24166", "bd03de2d-8858-4faf-bda1-6f37a0ee9919", "User", "USER" }
+            //    });
 
             migrationBuilder.CreateIndex(
                 name: "IX_ExtraClasses_TeacherId",
