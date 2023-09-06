136625a22050314c082b4223ee630c58adc42056
Trinh Quoc <draogon10a3@gmail.com>
2023-09-06 09:17:06 +0700

    Save configurations info into EV

---
+++ frontend/src/app/app.config.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+
+const result = dotenv.config({ path: "../../env" });
+
+export const { BASE_URL } = process.env;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,2 +1,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime.js";
+
+import { BASE_URL } from "./app/app.config.js";
+
+console.log(BASE_URL);
