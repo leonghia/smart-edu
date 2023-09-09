55f7a591850679598fa4058488e6cfa73d2c49fb
Nghia La <leonghiacnn@gmail.com>
2023-09-10 05:00:45 +0700

    Delete data.helper.js

--- frontend/src/app/helpers/data.helper.js
+++
@@ -1,6 +0,0 @@
-export const getToken = function () { // public string | null getToken
-    // Kiem tra xem token co tren trinh duyet khong
-    // neu co tra ve token: string
-    // neu khong tra ve null
-    return localStorage.getItem("token");
-}
