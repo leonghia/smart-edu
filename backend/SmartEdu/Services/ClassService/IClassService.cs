13789f0246f87b4d94518330653ee0fef33219b1
Nguyen Huong <nguyenhuongg1104@gmail.com>
2023-09-20 10:13:31 +0700

    EC unregister API

--- backend/SmartEdu/Services/ClassService/IClassService.cs
+++ backend/SmartEdu/Services/ClassService/IClassService.cs
@@ -1,8 +1,10 @@
 using SmartEdu.DTOs.EcBookmarkDTO;
+using SmartEdu.DTOs.ExtraClassStudentDTO;
 using SmartEdu.Models;
 
 namespace SmartEdu.Services.ClassService;
 
 public interface IClassService {
     Task<ServerResponse<object>> UnBookmark(DeleteExtraClassEcBookmarkDTO deleteExtraClassEcBookmarkDTO);
+    Task<ServerResponse<object>> UnRegister(DeleteExtraClassStudentDTO deleteExtraClassStudentDTO); 
 }
